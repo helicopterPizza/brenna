0fc4c9e79c7dcc7cbc8191a02093f2230267e9c0
Brendan Blais <brendanjoblais@gmail.com>
2024-08-09 14:32:36 -0400

    Delegated decision making to TestCoordinator class

--- brenna/python/main.py
+++ brenna/python/main.py
@@ -1,10 +1,29 @@
 import time
-
 from playwright.sync_api import sync_playwright
-import actions as actions
+from TestCoordinator import TestCoordinator
 
-with sync_playwright() as p:
-    browserInst, page = actions.launch(p, 0, "https://www.google.ca")
-    actions.clickElementByText("Privacy", page)
+action = {
+    "action": "click",
+    "selectorType": "text",
+    "selectorContent": "Privacy"
+}
+instruct = {}
+instruct[0] = action
+action = {
+    "action": "click",
+    "selectorType": "text",
+    "selectorContent": "Privacy"
+}
+instruct[1] = action
+def RunSuite(instructions):
+    with sync_playwright() as p:
 
-    browserInst.close()
+        tc = TestCoordinator(p)
+        for step in range(len(instructions)):
+            tc.chooseAction(instructions[step])
+            time.sleep(5)
+
+        time.sleep(10)
+        tc.close()
+
+RunSuite(instruct)
